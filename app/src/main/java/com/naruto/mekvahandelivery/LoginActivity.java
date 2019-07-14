ec2976811b12fcf8f29335e83ed483229f2a4b01
amankriet <amankriet@bitbucket.org>
2019-07-14 14:26:58 +0530

    first commit inrepository

---
+++ app/src/main/java/com/naruto/mekvahandelivery/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.naruto.mekvahandelivery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void loginSubmit(View view) {
+        Intent intent = new Intent(LoginActivity.this, NavActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
