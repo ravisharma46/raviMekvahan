21ca016ef1b40384f6e178afacb20320551f63dc
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-20 17:33:40 +0530

    Update ScanQrcode.java

--- MekvahanDelivery/app/src/main/java/com/naruto/mekvahandelivery/ScanQrcode.java
+++ MekvahanDelivery/app/src/main/java/com/naruto/mekvahandelivery/ScanQrcode.java
@@ -1,21 +1,37 @@
 package com.naruto.mekvahandelivery;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ScanQrcode extends AppCompatActivity {
+
+
+
+public class ScanQrcode extends AppCompatActivity  {
+
+   private Button scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrcode);
+
+        scan=findViewById(R.id.btscan);
 
 
         try{
@@ -26,11 +42,26 @@ public class ScanQrcode extends AppCompatActivity {
             upArrow.setColorFilter(getResources().getColor(R.color.chart_deep_red), PorterDuff.Mode.SRC_ATOP);
             getSupportActionBar().setHomeAsUpIndicator(upArrow);
         }
-
         catch(Exception e){
             e.printStackTrace();
         }
+
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ScanQrcode.this,CaptureActivityPortrait.class);
+               startActivity(i);
+            }
+        });
+
+
+
+
     }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,4 +80,7 @@ public class ScanQrcode extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+
+
 }
