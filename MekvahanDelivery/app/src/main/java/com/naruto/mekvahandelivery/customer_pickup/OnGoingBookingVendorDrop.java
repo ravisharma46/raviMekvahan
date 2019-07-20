77bfcd8536252052ea795dfd15b2acd5db4d4423
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-20 17:33:34 +0530

    Update OnGoingBookingVendorDrop.java

--- MekvahanDelivery/app/src/main/java/com/naruto/mekvahandelivery/customer_pickup/OnGoingBookingVendorDrop.java
+++ MekvahanDelivery/app/src/main/java/com/naruto/mekvahandelivery/customer_pickup/OnGoingBookingVendorDrop.java
@@ -2,31 +2,50 @@ package com.naruto.mekvahandelivery.customer_pickup;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import com.ceylonlabs.imageviewpopup.ImagePopup;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.naruto.mekvahandelivery.R;
 import com.naruto.mekvahandelivery.customer_report.AddCustomerReport;
 import com.naruto.mekvahandelivery.customer_report.ViewCustomerReport;
+import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.dropConfirm;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class OnGoingBookingVendorDrop extends AppCompatActivity {
     private LinearLayout paint_linear;
     private TextView tvDetails, date, time;
-    private ImageView call;
-    private Button report_show;
+    private ImageView call,iv_qrcode;
+    private Button report_show,drop;
 
 
     @Override
@@ -40,6 +59,42 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
         report_show=findViewById(R.id.report_view);
 
         call = findViewById(R.id.call);
+        iv_qrcode=findViewById(R.id.iv_qrcode);
+        drop=findViewById(R.id.bt_drop);
+
+       generateQrcode();
+
+
+        final ImagePopup imagePopup = new ImagePopup(this);
+        imagePopup.setWindowHeight(800); // Optional
+        imagePopup.setWindowWidth(800); // Optional
+       imagePopup.setBackgroundColor(Color.BLACK);
+        imagePopup.setFullScreen(true); // Optional
+        imagePopup.setHideCloseIcon(true);  // Optional
+        imagePopup.setImageOnClickClose(true);  // Optional
+
+
+        imagePopup.initiatePopup(iv_qrcode.getDrawable());
+
+        iv_qrcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imagePopup.viewPopup();
+            }
+        });
+
+        drop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dropConfirm(OnGoingBookingVendorDrop.this);
+            }
+        });
+
+
+
+
+
+
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -85,6 +140,9 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
 
 
     }
+
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -102,6 +160,65 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+
+    public void generateQrcode(){
+        try {
+            //setting size of qr code
+            int width =300;
+            int height = 300;
+            int smallestDimension = width < height ? width : height;
+
+
+            String qrCodeData = "123456";
+            //setting parameters for qr code
+            String charset = "UTF-8";
+            Map<EncodeHintType, ErrorCorrectionLevel> hintMap =new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            CreateQRCode(qrCodeData, charset, hintMap, smallestDimension, smallestDimension);
+
+        } catch (Exception ex) {
+            Log.e("QrGenerate",ex.getMessage());
+        }
+    }
+
+    public  void CreateQRCode(String qrCodeData, String charset, Map hintMap, int qrCodeheight, int qrCodewidth){
+
+
+        try {
+            //generating qr code in bitmatrix type
+            BitMatrix matrix = new MultiFormatWriter().encode(new String(qrCodeData.getBytes(charset), charset),
+                    BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
+            //converting bitmatrix to bitmap
+
+            int width = matrix.getWidth();
+            int height = matrix.getHeight();
+            int[] pixels = new int[width * height];
+            // All are 0, or black, by default
+            for (int y = 0; y < height; y++) {
+                int offset = y * width;
+                for (int x = 0; x < width; x++) {
+                    //pixels[offset + x] = matrix.get(x, y) ? BLACK : WHITE;
+                    pixels[offset + x] = matrix.get(x, y) ?
+                            ResourcesCompat.getColor(getResources(),R.color.chart_deep_red,null) :Color.WHITE;
+                }
+            }
+
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+            //setting bitmap to image view
+
+            Bitmap overlay = BitmapFactory.decodeResource(getResources(), R.drawable.mek_logo);
+
+           // iv_qrcode.setImageBitmap(mergeBitmaps(overlay,bitmap));
+            iv_qrcode.setImageBitmap(bitmap);
+
+        }catch (Exception er){
+            Log.e("QrGenerate",er.getMessage());
+        }
+    }
+
+
 }
 
 
